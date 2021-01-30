0fa51cea432e1d66cfdb4fcd36f7fe16cb120455
= <=>
2021-01-30 22:43:48 +0530

    Basic server running on port 8888

--- firstapp/app.js
+++ firstapp/app.js
@@ -1,7 +1,9 @@
 const http = require('http')
 
 const server = http.createServer(function(req, res){
-    console.log(req)
+    res.setHeader('Content-Type','text/html')
+    res.write('Welcome')
+    res.end()
 })
 
 server.listen(8888)
