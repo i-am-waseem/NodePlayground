b6baaf7ea1c6a5a88b238b3e1c5192bbaef5733f
= <=>
2021-01-31 12:41:16 +0530

    some changes to server

--- firstapp/app.js
+++ firstapp/app.js
@@ -1,8 +1,24 @@
 const http = require('http')
 
 const server = http.createServer(function(req, res){
+    const url = req.url
     res.setHeader('Content-Type','text/html')
-    res.write('Welcome')
+
+    if(url === '/'){
+        console.log('home request')
+        res.write('Welcome, Home!')
+        return res.end()
+    }
+
+    if(url ==='/message' && req.method === 'POST'){
+        console.log('post request')
+        res.statusCode = 302
+        res.setHeader('location','/')
+        return res.end()
+    }
+
+    res.setHeader('Content-Type','text/html')
+    res.write('Default Page')
     res.end()
 })
 
